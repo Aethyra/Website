ba115a1115d26f762ed5cd02ace7deed952b1be7
bels <bels11@yahoo.com>
2010-10-01 20:59:15 -0400

    - Fixed some styling problems on the download page

--- downloads.pl
+++ downloads.pl
@@ -7,7 +7,30 @@ use DBI;
 my @styles = ("styles/styles.css");
 my @javascripts = ();
 
-my $vars = {'title' => 'Aethyra Project - Downloads','styles' => \@styles,'javascripts' => \@javascripts};
+my $config = "/var/aethyra/config";
+
+#### read in config ####
+open (CONFIG,$config);
+my @config = <CONFIG>;
+my @line = split(":",$config[0]);
+my $online_file = $line[1];
+chomp($online_file);
+@line = split(":",$config[1]);
+my $credential_file = $line[1];
+chomp($credential_file);
+close(CONFIG);
+#### END CONFIG ####
+
+
+#### get online players #####
+open (ONLINE,$online_file);
+my @array = <ONLINE>;
+my $online = pop(@array);
+chomp $online;
+close(ONLINE);
+#####################
+
+my $vars = {'title' => 'Aethyra Project - Downloads','styles' => \@styles,'javascripts' => \@javascripts,'online_players' => $online};
 
 my $file = "downloads.tt";
 
